c9844b8b7776969fcbade6e63b7646c750771b2c
Taivas Gogoljuk <taivas.gogoljuk@gmail.com>
2020-08-26 19:29:18 +0300

    autogit

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const port = 3000
 const fs = require("fs")
 
 app.use(express.static('public'))
-    
-
-
+  
 app.listen(port, function() {
     console.log(`Example app listening at http://localhost:${port}`)
 })
